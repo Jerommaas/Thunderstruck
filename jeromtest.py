8e49c24554c8c7992ae5393195c04d8d1aed4f66
jorstap <42149380+jorstap@users.noreply.github.com>
2018-08-06 19:07:54 +0200

    added custom amera controls

--- jeromtest.py
+++ jeromtest.py
@@ -1,7 +1,9 @@
+from math import pi, sin, cos
+ 
 from direct.showbase.ShowBase import ShowBase
+from direct.task import Task
  
 class MyApp(ShowBase):
- 
     def __init__(self):
         ShowBase.__init__(self)
  
@@ -13,6 +15,16 @@ class MyApp(ShowBase):
         self.scene.setScale(0.25, 0.25, 0.25)
         self.scene.setPos(-8, 42, 0)
  
+        # Add the spinCameraTask procedure to the task manager.
+        self.taskMgr.add(self.spinCameraTask, "SpinCameraTask")
+ 
+    # Define a procedure to move the camera.
+    def spinCameraTask(self, task):
+        angleDegrees = task.time * 6.0
+        angleRadians = angleDegrees * (pi / 180.0)
+        self.camera.setPos(20 * sin(angleRadians), -20.0 * cos(angleRadians), 3)
+        self.camera.setHpr(angleDegrees, 0, 0)
+        return Task.cont
  
 app = MyApp()
 app.run()
