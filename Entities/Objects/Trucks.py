7f73afe7f0008e321cb27c2c08813f3d057b9e19
Jerom Maas <jerommaas@gmail.com>
2018-08-14 19:36:53 +0200

    Wip

---
+++ Entities/Objects/Trucks.py
@@ -0,0 +1,12 @@
+
+modelfolder = "Entities/Objects/Models/"
+class Truck01:
+    eggname = modelfolder+"Truck01/truck_01.egg"
+    def __init__(self):
+        self.Model()
+
+    def Model(self):
+        self.m = loader.loadModel(self.eggname)
+        self.m.reparentTo(render)
+        self.m.setPos(0,0,10)
+        self.m.setHpr(0,90,0)
