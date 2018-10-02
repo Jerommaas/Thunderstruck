d94734b64a1dd6abdfd3141bb732ead823aed36b
jorstap <42149380+jorstap@users.noreply.github.com>
2018-10-02 23:44:20 +0200

    fixed crap ofzo

--- Entities/skyDome.py
+++ Entities/skyDome.py
@@ -16,23 +16,36 @@ class skyDome:
         # note: SkySphere.bam has to be be re-created for each Panda3D version. you can do so by running sky Sphere.py
 
         # load inverted sphere model, to texture
-        self.SkyDome = loader.loadModel("Entities/Maps/skydome1/InvertedSphere.egg")
+        #self.SkyDome = loader.loadModel("Entities/Maps/skydome1/InvertedSphere.egg")
+        self.SkyDome = loader.loadModel("Entities/Maps/skydome1/spacedome.egg")
         # create 3D texture coordinates on sphere
-        self.SkyDome.setTexGen(TextureStage.getDefault(), TexGenAttrib.MWorldPosition)
-        self.SkyDome.setTexProjector(TextureStage.getDefault(), render, self.SkyDome)
-        #self.SkyDome.setTexGen(TextureStage.getDefault(), TexGenAttrib.MWorldPosition)
-        #self.SkyDome.setTexProjector(TextureStage.getDefault(), render, self.SkyDome)
+        #self.SkyDome.setTexGen(TextureStage.getDefault(), TexGenAttrib.MWorldPosition) #* Copies the (x, y, z) position of each vertex, in world space, to the (u, v, w) texture coordinates.
+        #self.SkyDome.setTexGen(TextureStage.getDefault(), TexGenAttrib.MEyePosition) # Copies the (x, y, z) position of each vertex, in camera space, to the (u, v, w) texture coordinates.
+        #self.SkyDome.setTexGen(TextureStage.getDefault(), TexGenAttrib.MWorldNormal) # Copies the (x, y, z) lighting normal of each vertex, in world space, to the (u, v, w) texture coordinates.
+        #self.SkyDome.setTexGen(TextureStage.getDefault(), TexGenAttrib.MEyeNormal) # Copies the (x, y, z) lighting normal of each vertex, in camera space, to the (u, v, w) texture coordinates.
+        #self.SkyDome.setTexGen(TextureStage.getDefault(), TexGenAttrib.MEyeSphereMap) #* Generates (u, v) texture coordinates based on the lighting normal and the view vector to apply a standard reflection sphere map.
+        #self.SkyDome.setTexGen(TextureStage.getDefault(), TexGenAttrib.MEyeCubeMap) # Generates (u, v, w) texture coordinates based on the lighting normal and the view vector to apply a standard reflection cube map.
+        #self.SkyDome.setTexGen(TextureStage.getDefault(), TexGenAttrib.MWorldCubeMap) # Generates (u, v, w) texture coordinates based on the lighting normal and the view vector to apply a standard reflection cube map.
+
+
+
+        #self.SkyDome.setTexProjector(TextureStage.getDefault(), render, self.SkyDome) # should only be needed when projectibf cube map to sphere...
 
         # create a cube map texture from 6 separate textures: (# should run 0-5)
-        tex = loader.loadCubeMap('Entities/Maps/skydome1/lakes_#.png')
-        #tex2 = loader.loadCubeMap('Entities/Maps/skydome1/lakes_#_bloom.png')
-        ts = TextureStage('ts')
-        self.SkyDome.setTexGen(ts, TexGenAttrib.MWorldPosition)
-        self.SkyDome.setTexProjector(ts, render, self.SkyDome)
-        ts.setMode(TextureStage.MModulateGlow)
+        #tex = loader.loadCubeMap('Entities/Maps/skydome1/lakes_#.png')
+        # or: get a pre-wrapped texture from the interwebs
+        scene = loader.loadTexture('Entities/Maps/skydome1/14-Hamarikyu_Bridge_B_8k.jpg') #new
+        
+
+        #ts = TextureStage('ts')
+        #self.SkyDome.setTexGen(ts, TexGenAttrib.MWorldPosition) # old
+        #self.SkyDome.setTexGen(ts, TexGenAttrib.MEyeSphereMap) # new
+        #self.SkyDome.setTexProjector(ts, render, self.SkyDome) # old
+        # ts.setMode(TextureStage.MModulateGlow) # old
+
         # and give it to inverted sphere
-        #self.SkyDome.setTexture(TextureStage.getDefault(),tex)
-        self.SkyDome.setTexture(ts,tex)
+        #self.SkyDome.setTexture(TextureStage.getDefault(),scene)
+        #self.SkyDome.setTexture(ts,tex)
 
         #TODO: make sure that this cube map and .eeg model are loaded from a BAM file for faster loading. (and don't forget to re-set textProjector after loading!)
         # load model (sphere + texture)
@@ -52,21 +65,21 @@ class skyDome:
         self.SkyDome.set_bin("background", 0)
         self.SkyDome.set_depth_write(0)
         # ignore light effects?
-        #self.SkyDome.setLightOff()
+        self.SkyDome.setLightOff()
 
+        #base.oobe()
+        #render.setShaderAuto()
+        #filters = CommonFilters(base.win, base.cam)
+        #filterok = filters.setBloom(blend=(0, 0, 0, 1), desat=-0.5, mintrigger =0.1, intensity=8.0, size="medium")
 
-        render.setShaderAuto()
-        filters = CommonFilters(base.win, base.cam)
-        filterok = filters.setBloom(blend=(0, 0, 0, 1), desat=-0.5, mintrigger =0.1, intensity=8.0, size="medium")
-
-        # ass some light
+        # add some light
         
         dlight = DirectionalLight('dlight')
         alight = AmbientLight('alight')
         dlnp = render.attachNewNode(dlight)
         alnp = render.attachNewNode(alight)
-        dlight.setColor((0.2, 0.7, 0.2, 1))
-        alight.setColor((0.2, 0.2, 0.2, 1))
-        dlnp.setHpr(0, -60, 0)
+        dlight.setColor((0.2, 0.2, 0.2, 1))
+        alight.setColor((0.7, 0.7, 0.7, 1))
+        dlnp.setHpr(60, -60, 0)
         render.setLight(dlnp)
         render.setLight(alnp)
