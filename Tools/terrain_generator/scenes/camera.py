ec0bfabd5b411fea9fffa3751b178165df9835b3
Victor <victordieben@gmail.com>
2018-11-05 22:28:19 +0100

    - removed old files

--- Tools/terrain_generator/scenes/camera.py
+++ Tools/terrain_generator/scenes/camera.py
@@ -30,7 +30,8 @@ class Camera(object):
 
         self.control = np.array( [0,0] )
 
-        # self.world.camera.node().getLens().setFov(120)
+        # print( self.world.camera) 
+        # self.world.camera.setFov(120) 
 
         # camera control
         taskMgr.add(self.tick, 'TickCameraMovement')
@@ -41,30 +42,47 @@ class Camera(object):
     def move_y(self, y):
         self.control[1] = y
 
+    def get_forward_vector(self):
+        cam = self.world.camera
+        yaw = np.deg2rad( cam.get_h() )
+        # pitch = np.deg2rad(  cam.get_p() )
+        # return np.array( [np.cos(yaw) , np.sin(yaw), 0 ])
+        return self.world.camera.getQuat().getForward()
+
+    def get_right_vector(self):
+        forward = self.get_forward_vector()
+        right = np.cross( forward, np.array([0,0,1]) )
+        return right / np.linalg.norm( right )
+
+
+
 
     def tick(self, task ):
         t_cur = time.time()
         dt = t_cur - self.time
         cam = self.world.camera
 
-        vmax = 1
+        vmax = 3.6
 
         yaw_deg = cam.get_h()
         pitch_deg = cam.get_p()
         roll_deg = cam.get_r()
 
-        yaw = np.pi * yaw_deg  / 180
-        pitch = np.pi * pitch_deg  / 180
-        roll = np.pi * roll_deg  / 180
+        yaw = np.pi * yaw_deg  / 180.
+        pitch = np.pi * pitch_deg  / 180.
+        roll = np.pi * roll_deg  / 180.
 
         # cam.set_p( t_cur )
         f = np.pi / 2
         forward = np.array( [np.cos(yaw), np.sin(yaw), 0 ]) # TODO(victor): calculate correct vectors
         right = np.array( [np.cos(yaw+f), np.sin(yaw+f), 0])
  
-        self.pos = self.pos + (dt * self.control[0] * forward) # add forward movement
-        self.pos = self.pos + (dt * self.control[1] * right) # add forward movement
+        # self.pos = self.pos + (dt * self.control[0] * right  )
+        # self.pos = self.pos + (dt * self.control[1] * forward)
 
+        self.pos = self.pos + (dt * self.control[0] * self.get_right_vector()  )
+        self.pos = self.pos + (dt * self.control[1] * self.get_forward_vector() )
+        
         # cam.setPos( *self.pos ) 
         cam.set_x( self.pos[0] )
         cam.set_y( self.pos[1] )
--- Tools/terrain_generator/scenes/picker.py
+++ Tools/terrain_generator/scenes/picker.py
@@ -6,12 +6,37 @@
 
 
 class picker(object):
-    def __init__(self, mouse, render):
+    def __init__(self, mouse, render, camera):
         self.mouse = mouse
         self.render = render 
+        self.camera = camera
+        # see:
+        # http://www.panda3d.org/manual/index.php/Clicking_on_3D_Objects
+        
+        self.pickerNode = CollisionNode('mouseRay')
+        pickerNP = camera.attachNewNode(self.pickerNode)
+        self.pickerNode.setFromCollideMask(GeomNode.getDefaultCollideMask())
+        self.pickerRay = CollisionRay()
+        self.pickerNode.addSolid(self.pickerRay)
+        self.traverser.addCollider(self.pickerNP, self.handle_picker() )
 
         
+    def handle_picker(self):
+        self.set_picker_ray()
+        self.traverser.traverse(self.render)
+        # Assume for simplicity's sake that myHandler is a CollisionHandlerQueue.
+        if myHandler.getNumEntries() > 0:
+            # This is so we get the closest object
+            myHandler.sortEntries()
+            pickedObj = myHandler.getEntry(0).getIntoNodePath()
 
+    def set_picker_ray(self):
+        # First we check that the mouse is not outside the screen.
+        if not base.mouseWatcherNode.hasMouse():
+            return
+
+        mpos = base.mouseWatcherNode.getMouse()
+        self.pickerRay.setFromLens(base.camNode, mpos.getX(), mpos.getY())
 
 
     
